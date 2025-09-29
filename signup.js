3613d81c7c5869c5aa73254db3a30fc17019e8e6
Bohdan <bohdansusaiev@gmail.com>
2025-09-29 21:21:14 +0300

    feat: додано валідацію email

--- signup.js
+++ signup.js
@@ -3,3 +3,7 @@ function signup(username, email, password) {
   console.log('Реєстрація користувача:', username);
   return { success: true };
 }
+
+function validateEmail(email) {
+  return email.includes('@');
+}
